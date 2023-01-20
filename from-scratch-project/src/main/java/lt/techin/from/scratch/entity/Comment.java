6387b5d9a9011786b1db20eac181822cef746342
IndreSu <115785979+IndreSu@users.noreply.github.com>
2023-01-20 10:42:47 +0200

    Add files via upload

---
+++ from-scratch-project/src/main/java/lt/techin/from/scratch/entity/Comment.java
@@ -0,0 +1,51 @@
+package lt.techin.from.scratch.entity;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+
+@Entity
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+
+    @ManyToOne (fetch = FetchType.EAGER, cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+//    @JoinColumn(name = "entity_id")
+    private Note comment;
+
+    public Comment() {
+    }
+
+    public Comment(Long id, String text, Note comment) {
+        this.id = id;
+        this.text = text;
+        this.comment = comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Note getComment() {
+        return comment;
+    }
+
+    public void setComment(Note comment) {
+        this.comment = comment;
+    }
+}
